b82d657173f10b91879775dda4b420ce6d946516
Marcin Sawicki <marcin.sawicki@applover.pl>
2023-04-08 15:00:01 +0200

    Re-enable indexing for title and summary fields

--- semantic_search/mappings.py
+++ semantic_search/mappings.py
@@ -1,8 +1,8 @@
 UNIVERSAL_SENTENCE_ENCODER_MAPPING = {
     'mappings': {
         'properties': {
-            'title': {'type': 'text', 'index': False},
-            'summary': {'type': 'text', 'index': False},
+            'title': {'type': 'text', 'index': True},
+            'summary': {'type': 'text', 'index': True},
             'content': {'type': 'text', 'index': False},
             'title_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
             'summary_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
