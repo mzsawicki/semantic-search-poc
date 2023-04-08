06081c856e0ded53991ceefac8f7057b34332f2d
Marcin Sawicki <marcin.sawicki@applover.pl>
2023-04-08 14:27:53 +0200

    Update mappings to index vector fields

--- semantic_search/mappings.py
+++ semantic_search/mappings.py
@@ -4,9 +4,9 @@ UNIVERSAL_SENTENCE_ENCODER_MAPPING = {
             'title': {'type': 'text'},
             'summary': {'type': 'text'},
             'content': {'type': 'text'},
-            'title_embed': {'type': 'dense_vector', 'dims': 512},
-            'summary_embed': {'type': 'dense_vector', 'dims': 512},
-            'content_embed': {'type': 'dense_vector', 'dims': 512}
+            'title_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
+            'summary_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
+            'content_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'}
         }
     }
 }
