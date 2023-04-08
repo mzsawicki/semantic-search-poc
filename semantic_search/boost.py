7ae7bea9300f1ebbcb66d559293d619c9a08677b
Marcin Sawicki <marcin.sawicki@applover.pl>
2023-04-08 14:27:29 +0200

    Add search boost value definitions

---
+++ semantic_search/boost.py
@@ -0,0 +1,9 @@
+class Boost:
+    NO_BOOST = 1
+
+    title = NO_BOOST
+    summary = NO_BOOST
+    content = NO_BOOST
+    title_embed = NO_BOOST
+    summary_embed = NO_BOOST
+    content_embed = NO_BOOST
