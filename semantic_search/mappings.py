edceeac9ae12594074d88996e2c3c0a99fc9cb8a
Marcin Sawicki <marcin.sawicki@applover.pl>
2023-04-08 14:52:11 +0200

    Disable indexing for plaintext fields

--- semantic_search/mappings.py
+++ semantic_search/mappings.py
@@ -1,9 +1,9 @@
 UNIVERSAL_SENTENCE_ENCODER_MAPPING = {
     'mappings': {
         'properties': {
-            'title': {'type': 'text'},
-            'summary': {'type': 'text'},
-            'content': {'type': 'text'},
+            'title': {'type': 'text', 'index': False},
+            'summary': {'type': 'text', 'index': False},
+            'content': {'type': 'text', 'index': False},
             'title_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
             'summary_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'},
             'content_embed': {'type': 'dense_vector', 'dims': 512, 'index': True, 'similarity': 'cosine'}
